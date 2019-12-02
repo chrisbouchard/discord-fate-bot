ba337342b630071e6def79c59be36e94593b8682
Chris Bouchard <chris@upliftinglemma.net>
2019-12-02 08:50:28 -0500

    Write a basic bot
    Doesn't do much for now. Needs more modules.

---
+++ discord_fate_bot/__init__.py
@@ -0,0 +1,35 @@
+import discord
+import environ
+import logging
+import os
+
+from . import config
+
+bot_config = environ.to_config(config.BotConfig, os.environ)
+
+if bot_config.log_level is not None:
+    logging.basicConfig(level=bot_config.log_level)
+
+logger = logging.getLogger('discord_fate_bot')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    logger.debug('READY: Logged in as %s', client.user)
+
+@client.event
+async def on_message(message):
+    logger.debug('MESSAGE: Received message: %s', message)
+
+    # Don't get into a feedback loop!
+    if message.author == client.user:
+        return
+
+    # Otherwise respond politely.
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+# TODO: Get a real token. (From the environment?)
+client.run(bot_config.token)
+
---
+++ discord_fate_bot/config.py
@@ -0,0 +1,14 @@
+import environ
+import logging
+
+def log_level_numeric(log_level):
+    numeric_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f'Invalid log level: {log_level}')
+    return numeric_level
+
+@environ.config(prefix='DISCORD_FATE_BOT')
+class BotConfig:
+    log_level = environ.var('INFO', converter=log_level_numeric)
+    token = environ.var()
+
