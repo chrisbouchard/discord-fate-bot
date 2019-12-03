6e2e72b7a0ca757a06fde1f7d0a1f6d5378da3d1
Chris Bouchard <chris@upliftinglemma.net>
2019-12-02 20:11:58 -0500

    Refactor to cog and implement roll_vs for opposition

--- discord_fate_bot/roll_ext.py
+++ discord_fate_bot/roll_ext.py
@@ -1,3 +1,4 @@
+import discord
 import re
 
 from discord.ext import commands
@@ -14,17 +15,45 @@ class Modifier(commands.Converter):
             raise commands.CommandError("Modifier must have format: {+|-}VALUE")
         return int(argument)
 
-@commands.command(aliases=['r'])
-async def roll(ctx, modifier: Modifier = 0):
-    roll = DICE_POOL.roll()
-    shifts = roll.total() + modifier
 
-    reply = ctx.message.author.mention
-    original_command = ctx.message.content
+class RollCog(commands.Cog):
+    @commands.command(aliases=['r'])
+    async def roll(self, ctx, modifier: Modifier = 0):
+        player = ctx.message.author
+        original_command = ctx.message.content
 
-    await ctx.send(f'{reply} Results for `{original_command}`:\n```\n{roll}```\nGenerated **{shifts:+}** shifts')
+        roll = DICE_POOL.roll()
+        shifts = roll.total() + modifier
+
+        results_header = f'{player.mention} Results for `{original_command}`:\n'
+        results_roll = f'```\n{roll}```\n'
+        results_final = f'You generated **{shifts:+}** shifts'
+
+        await ctx.send(f'{results_header}{results_roll}{results_final}')
+
+    @commands.command(aliases=['rv'])
+    async def roll_vs(self, ctx, modifier: Optional[Modifier] = 0, opposition: int = 0):
+        player = ctx.message.author
+        original_command = ctx.message.content
+
+        roll = DICE_POOL.roll()
+        shifts = roll.total() + modifier - opposition
+
+        results_header = f'{player.mention} Results for `{original_command}`:\n'
+        results_roll = f'```\n{roll}```\n'
+
+        if shifts >= 3:
+            results_final = f'You **succeeded with style** with **{shifts:+}** shifts!'
+        elif shifts > 0:
+            results_final = f'You **succeeded** with **{shifts:+}** shifts.'
+        elif shifts < 0:
+            results_final = f'You **failed** with **{shifts:+}** shifts.'
+        else:
+            results_final = "You **tied**."
+
+        await ctx.send(f'{results_header}{results_roll}{results_final}')
 
 
 def setup(bot):
-    bot.add_command(roll)
+    bot.add_cog(RollCog())
 
