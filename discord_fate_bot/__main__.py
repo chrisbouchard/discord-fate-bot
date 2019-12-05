6bc5e51d59d37f5404cfa12c4c1496c823c6f80b
Chris Bouchard <chris@upliftinglemma.net>
2019-12-05 14:35:18 -0500

    Reorganize and use relative imports

--- discord_fate_bot/__main__.py
+++ discord_fate_bot/__main__.py
@@ -3,8 +3,9 @@ import logging
 import os
 
 from discord.ext.commands import Bot
+from importlib.util import resolve_name
 
-from discord_fate_bot import config
+from .model import config
 
 bot_config = environ.to_config(config.BotConfig, os.environ)
 
@@ -12,9 +13,9 @@ if bot_config.log_level is not None:
     logging.basicConfig(level=bot_config.log_level)
 
 bot = Bot(command_prefix = '!')
-bot.load_extension('discord_fate_bot.error_handling_ext')
-bot.load_extension('discord_fate_bot.aspects_ext')
-bot.load_extension('discord_fate_bot.roll_ext')
+bot.load_extension(resolve_name('.extensions.error_handling', __package__))
+bot.load_extension(resolve_name('.extensions.aspect', __package__))
+bot.load_extension(resolve_name('.extensions.roll', __package__))
 
 bot.run(bot_config.token)
 
