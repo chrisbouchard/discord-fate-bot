2924aad3abecb58dae848199a848194936855a84
Chris Bouchard <chris@upliftinglemma.net>
2019-12-10 21:08:44 -0500

    Reimplement !rollvs

--- discord_fate_bot/extensions/roll.py
+++ discord_fate_bot/extensions/roll.py
@@ -1,10 +1,12 @@
+import asyncio
 import re
 
+from collections import defaultdict
 from dataclasses import dataclass
 from discord import Member, TextChannel
 from discord.abc import User
-from discord.ext.commands import BadArgument, Cog, Converter, command
-from typing import Dict, FrozenSet, Optional, OrderedDict
+from discord.ext.commands import BadArgument, Cog, Converter, command, group
+from typing import Dict, FrozenSet, Optional, Tuple
 
 from ..dice import FateDiePool, Roll, RollContext
 
@@ -19,21 +21,68 @@ class Modifier(Converter):
         return int(argument)
 
 
-@dataclass(frozen = True)
-class PendingVsKey:
-    channel_id: TextChannel
-    user_ids: FrozenSet[User]
+class PendingVs:
+    waiting: Dict[Tuple[TextChannel, User], Dict[User, RollContext]]
 
-PendingVs = OrderedDict[User, RollContext]
+    def __init__(self):
+        self.waiting = defaultdict(dict)
+
+    def add_context(self, channel: TextChannel, player: User, opponent: User, context: RollContext):
+        key = (channel, player)
+        self.waiting[key][opponent] = context
+
+    def remove_context(self, channel: TextChannel, player: User, opponent: User):
+        key = (channel, player)
+
+        if not key in self.waiting:
+            return
+
+        waiting_for_player = self.waiting[key]
+
+        if opponent not in waiting_for_player:
+            return
+
+        del waiting_for_player[opponent]
+
+        if not waiting_for_player:
+            del self.waiting[key]
+
+    def pop_opponent_context(self, channel: TextChannel, player: User, opponent: User):
+        key = (channel, opponent)
+
+        if key not in self.waiting:
+            return None
+
+        waiting_for_opponent = self.waiting[key]
+
+        if player not in waiting_for_opponent:
+            return None
+
+        opponent_context = waiting_for_opponent.pop(player)
+
+        if not waiting_for_opponent:
+            del self.waiting[key]
+
+        return opponent_context
+
+    def get_all_pending(self, channel: TextChannel, player: User):
+        key = (channel, player)
+
+        if key not in self.waiting:
+            return []
+
+        return list(self.waiting[key].keys())
 
 
 class RollCog(Cog, name = 'Rolling'):
     """Commands for rolling dice."""
 
-    pending_vs: Dict[PendingVsKey, PendingVs]
+    pending_vs: PendingVs
+    vs_lock: asyncio.Lock
 
     def __init__(self):
-        self.pending_vs = dict()
+        self.pending_vs = PendingVs()
+        self.vs_lock = asyncio.Lock()
 
     @command(aliases = ['r'])
     async def roll(self, ctx, modifier: Optional[Modifier] = None, opposition: int = None):
@@ -53,40 +102,38 @@ class RollCog(Cog, name = 'Rolling'):
 
         await ctx.send(f'{results_header}\n{results_roll}\n{results_final}')
 
-    @command(aliases = ['rv'])
-    async def rollvs(self, ctx, opponent: User, modifier: Modifier = None):
+    @group(aliases = ['rv'], invoke_without_command = True)
+    async def rollvs(self, ctx, opponent: Member, modifier: Modifier = None):
         """Roll against another user.
 
-         Once both users have called this command, the rolls will be made."""
+        Once both users have called this command, the rolls will be made."""
         channel = ctx.channel
-        author = ctx.author
+        player = ctx.author
 
-        if author == opponent:
-            await ctx.send(f"{author.mention} You can't roll against yourself.")
+        if player == opponent:
+            await ctx.send(f"{player.mention} You can't roll against yourself.")
             return
 
-        pending_vs_key = PendingVsKey(
-            channel_id = channel,
-            user_ids = frozenset({author, opponent})
-        )
-
-        pending_vs = self.pending_vs.setdefault(pending_vs_key, OrderedDict())
-
-        context = RollContext(
+        player_context = RollContext(
             modifiers = (modifier, ) if modifier else ()
         )
 
-        pending_vs[author] = context
+        async with self.vs_lock:
+            opponent_context = self.pending_vs.pop_opponent_context(channel, player, opponent)
 
-        # If our opponent is not ready, we'll get them later.
-        if opponent not in pending_vs:
-            return
+            # If we're first, just add our context and bail.
+            if not opponent_context:
+                self.pending_vs.add_context(channel, player, opponent, player_context)
+                return
 
-        del self.pending_vs[pending_vs_key]
+        contexts = {
+            player: player_context,
+            opponent: opponent_context
+        }
 
         rolls = {
             user: DICE_POOL.roll(context)
-            for user, context in pending_vs.items()
+            for user, context in contexts.items()
         }
 
         results_header = f'Results for opposed roll:'
@@ -94,6 +141,23 @@ class RollCog(Cog, name = 'Rolling'):
         results_final = format_opposed_roll_result(rolls)
 
         await ctx.send(f'{results_header}\n\n{results_roll}\n{results_final}')
+
+    @rollvs.command()
+    async def who(self, ctx):
+        """List the users you have pending rolls against."""
+        channel = ctx.channel
+        player = ctx.author
+
+        async with self.vs_lock:
+            users = self.pending_vs.get_all_pending(channel, player)
+
+        if not users:
+            await ctx.send(f"{player.mention} You aren't waiting on any rolls.")
+            return
+
+        users_str = '\n'.join(f'    • {user.name}' for user in users)
+
+        await ctx.send(f'{player.mention} You are waiting on rolls from:\n{users_str}')
 
 
 def format_roll_result(roll: Roll):
