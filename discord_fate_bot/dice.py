384d1422444ac01b799dd030e6fed038941b2dd1
Chris Bouchard <chris@upliftinglemma.net>
2020-03-06 00:20:11 -0500

    Add Value and adjust formatting

--- discord_fate_bot/dice.py
+++ discord_fate_bot/dice.py
@@ -1,6 +1,7 @@
 import random
 
 from dataclasses import dataclass, field
+from functools import total_ordering
 from typing import Sequence
 
 from .util import join_as_columns
@@ -8,23 +9,65 @@ from .util import join_as_columns
 FATE_DIE_POOL_SIZE = 4
 
 @dataclass
-class RollContext:
-    modifiers: Sequence[int] = ()
-    opposition: int = None
+@total_ordering
+class Value:
+    raw_value: int
 
-    def is_opposed(self):
-        return self.opposition is not None
+    def __add__(self, other):
+        if isinstance(other, Value):
+            return Value(self.raw_value + other.raw_value)
+        elif isinstance(other, int):
+            return Value(self.raw_value + other)
+        return NotImplemented
+
+    def __radd__(self, other):
+        if isinstance(other, Value):
+            return Value(other.raw_value + self.raw_value)
+        elif isinstance(other, int):
+            return Value(other + self.raw_value)
+        return NotImplemented
+
+    def __sub__(self, other):
+        if isinstance(other, Value):
+            return Value(self.raw_value - other.raw_value)
+        elif isinstance(other, int):
+            return Value(self.raw_value - other)
+        return NotImplemented
+
+    def __rsub__(self, other):
+        if isinstance(other, Value):
+            return Value(other.raw_value - self.raw_value)
+        elif isinstance(other, int):
+            return Value(other - self.raw_value)
+        return NotImplemented
+
+    def __lt__(self, other):
+        if isinstance(other, Value):
+            return self.raw_value < other.raw_value
+        elif isinstance(other, int):
+            return self.raw_value < other
+        return NotImplemented
+
+    def __str__(self):
+        sign = '+' if self.raw_value >= 0 else '−'
+        return f'{sign}{abs(self.raw_value)}'
+
+
+@dataclass
+class RollContext:
+    modifiers: Sequence[Value] = ()
+    opposition: Value = Value(0)
 
     def total_modifier(self):
         return sum(self.modifiers)
 
     def total_opposition(self):
-        return self.opposition or 0
+        return self.opposition or Value(0)
 
 @dataclass
 class DieFace:
     label: str
-    value: int
+    value: Value
 
     def __str__(self):
         side = '─' * len(self.label)
@@ -46,6 +89,9 @@ class Roll:
     faces: Sequence[DieFace]
     context: RollContext
 
+    def dice_total(self):
+        return sum(face.value for face in self.faces)
+
     def total(self):
         return sum(face.value for face in self.faces) + self.context.total_modifier()
 
@@ -57,15 +103,15 @@ class Roll:
         return join_as_columns(columns)
 
     def _str_tag(self):
-        tag = '\n'
+        tag = f'\n ({self.dice_total()})'
 
         if self.context.modifiers:
-            tag += ''.join(f' {modifier:+}' for modifier in self.context.modifiers)
+            tag += ''.join(f' {modifier}' for modifier in self.context.modifiers)
 
         tag += f'  =  {self.total()}'
 
         if self.context.opposition is not None:
-            tag += f'   vs  {self.context.opposition}'
+            tag += f'   vs   {self.context.opposition}'
 
         return tag
 
@@ -81,9 +127,9 @@ class FateDie(Die):
     def __init__(self):
         super().__init__(
             faces = (
-                DieFace(label = '−', value = -1),
-                DieFace(label = ' ', value = 0),
-                DieFace(label = '+', value = 1),
+                DieFace(label = '−', value = Value(-1)),
+                DieFace(label = ' ', value = Value(0)),
+                DieFace(label = '+', value = Value(1)),
             )
         )
 
--- discord_fate_bot/extensions/roll.py
+++ discord_fate_bot/extensions/roll.py
@@ -1,96 +1,53 @@
 import asyncio
 import re
 
-from collections import defaultdict
-from dataclasses import dataclass
-from discord import Member, TextChannel
-from discord.abc import User
-from discord.ext.commands import BadArgument, Cog, Converter, command, group
-from typing import Dict, FrozenSet, Optional, Tuple
+from discord.ext.commands import BadArgument, Cog, Converter, Greedy, command
+from typing import Optional
 
-from ..dice import FateDiePool, Roll, RollContext
+from ..dice import FateDiePool, Roll, RollContext, Value
 
 DICE_POOL = FateDiePool()
-MODIFIER_REGEX = re.compile(r'[+-]\d+')
 
 class Modifier(Converter):
     """A positive or negative roll modifier."""
     async def convert(self, ctx, argument):
-        if not MODIFIER_REGEX.fullmatch(argument):
+        if argument[:1] not in ('+', '-'):
             raise BadArgument("Modifier must have format: {+|-}VALUE")
-        return int(argument)
+        try:
+            return Value(int(argument))
+        except ValueError:
+            raise BadArgument("Modifier must be numeric")
 
+class Opposition(Converter):
+    """A numeric value."""
+    async def convert(self, ctx, argument):
+        try:
+            return Value(int(argument))
+        except ValueError:
+            raise BadArgument("Opposition must be numeric")
 
-class PendingVs:
-    waiting: Dict[Tuple[TextChannel, User], Dict[User, RollContext]]
-
-    def __init__(self):
-        self.waiting = defaultdict(dict)
-
-    def add_context(self, channel: TextChannel, player: User, opponent: User, context: RollContext):
-        key = (channel, player)
-        self.waiting[key][opponent] = context
-
-    def remove_context(self, channel: TextChannel, player: User, opponent: User):
-        key = (channel, player)
-
-        if not key in self.waiting:
-            return
-
-        waiting_for_player = self.waiting[key]
-
-        if opponent not in waiting_for_player:
-            return
-
-        del waiting_for_player[opponent]
-
-        if not waiting_for_player:
-            del self.waiting[key]
-
-    def pop_opponent_context(self, channel: TextChannel, player: User, opponent: User):
-        key = (channel, opponent)
-
-        if key not in self.waiting:
-            return None
-
-        waiting_for_opponent = self.waiting[key]
-
-        if player not in waiting_for_opponent:
-            return None
-
-        opponent_context = waiting_for_opponent.pop(player)
-
-        if not waiting_for_opponent:
-            del self.waiting[key]
-
-        return opponent_context
-
-    def get_all_pending(self, channel: TextChannel, player: User):
-        key = (channel, player)
-
-        if key not in self.waiting:
-            return []
-
-        return list(self.waiting[key].keys())
+class OppositionSigil(Converter):
+    """A separator."""
+    async def convert(self, ctx, argument):
+        if argument != 'vs':
+            raise BadArgument("Opposition must be numeric")
+        return None
 
 
 class RollCog(Cog, name = 'Rolling'):
     """Commands for rolling dice."""
 
-    pending_vs: PendingVs
-    vs_lock: asyncio.Lock
-
-    def __init__(self):
-        self.pending_vs = PendingVs()
-        self.vs_lock = asyncio.Lock()
-
     @command(aliases = ['r'])
-    async def roll(self, ctx, modifier: Optional[Modifier] = None, opposition: int = None):
+    async def roll(
+            self, ctx,
+            modifiers: Greedy[Modifier],
+            vs: Optional[OppositionSigil],
+            opposition: Optional[Opposition]):
         """Roll with an optional modifier and opposition."""
         player = ctx.author
 
         context = RollContext(
-            modifiers = (modifier, ) if modifier else (),
+            modifiers = tuple(modifiers),
             opposition = opposition
         )
 
@@ -102,87 +59,18 @@ class RollCog(Cog, name = 'Rolling'):
 
         await ctx.send(f'{results_header}\n{results_roll}\n{results_final}')
 
-    @group(aliases = ['rv'], invoke_without_command = True)
-    async def rollvs(self, ctx, opponent: Member, modifier: Modifier = None):
-        """Roll against another user.
-
-        Once both users have called this command, the rolls will be made."""
-        channel = ctx.channel
-        player = ctx.author
-
-        if player == opponent:
-            await ctx.send(f"{player.mention} You can't roll against yourself.")
-            return
-
-        player_context = RollContext(
-            modifiers = (modifier, ) if modifier else ()
-        )
-
-        async with self.vs_lock:
-            opponent_context = self.pending_vs.pop_opponent_context(channel, player, opponent)
-
-            # If we're first, just add our context and bail.
-            if not opponent_context:
-                self.pending_vs.add_context(channel, player, opponent, player_context)
-                return
-
-        contexts = {
-            player: player_context,
-            opponent: opponent_context
-        }
-
-        rolls = {
-            user: DICE_POOL.roll(context)
-            for user, context in contexts.items()
-        }
-
-        results_header = f'Results for opposed roll:'
-        results_roll = '\n'.join(f'{user.mention}:\n```{roll}```' for user, roll in rolls.items())
-        results_final = format_opposed_roll_result(rolls)
-
-        await ctx.send(f'{results_header}\n\n{results_roll}\n{results_final}')
-
-    @rollvs.command()
-    async def who(self, ctx):
-        """List the users you have pending rolls against."""
-        channel = ctx.channel
-        player = ctx.author
-
-        async with self.vs_lock:
-            users = self.pending_vs.get_all_pending(channel, player)
-
-        if not users:
-            await ctx.send(f"{player.mention} You aren't waiting on any rolls.")
-            return
-
-        users_str = '\n'.join(f'    • {user.name}' for user in users)
-
-        await ctx.send(f'{player.mention} You are waiting on rolls from:\n{users_str}')
-
 
 def format_roll_result(roll: Roll):
     shifts = roll.result()
 
-    if roll.context.is_opposed():
-        return f'You generated **{shifts:+}** shifts.'
-
     if shifts >= 3:
-        return f'You **succeeded with style** with **{shifts:+}** shifts!'
+        return f'You **succeeded with style** with **{shifts}** shifts!'
     elif shifts > 0:
-        return f'You **succeeded** with **{shifts:+}** shifts.'
+        return f'You **succeeded** with **{shifts}** shifts.'
     elif shifts < 0:
-        return f'You **failed** with **{shifts:+}** shifts.'
+        return f'You **failed** with **{shifts}** shifts.'
     else:
         return "You **tied**."
-
-def format_opposed_roll_result(rolls: Dict[User, Roll]):
-    loser, winner = sorted(rolls.keys(), key = lambda user: rolls[user].result())
-    shifts = rolls[winner].result() - rolls[loser].result()
-
-    if shifts > 0:
-        return f'{winner.mention} **won** by **{shifts}** shifts.'
-    else:
-        return f'{winner.mention} and {loser.mention} **tied**.'
 
 def setup(bot):
     bot.add_cog(RollCog())
