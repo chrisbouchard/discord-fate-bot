c1fe84e4ac26c8fd24b5ebfd1ca91fefc3dde1a8
Chris Bouchard <chris@upliftinglemma.net>
2020-03-21 23:24:41 -0400

    Refactor bot config validator
    I've moved the validator back into the BotConfig class. It seems better
    to have it near the fields it validates. It's a little weird still,
    because it's registered as a validator for token but also validates
    token_file, but there doesn't seem to be any way to register class-level
    validators.

--- discord_fate_bot/config.py
+++ discord_fate_bot/config.py
@@ -1,53 +1,56 @@
 import aiofiles
-import environ as _environ  # Renaming to avoid shadowing in read(environ)
+import environ
 import os
 
-@_environ.config
+@environ.config
 class BotConfig:
-    token = _environ.var(
+    token = environ.var(
         None,
-        help = 'The log-in token for the Discord bot account'
+        help = (
+            'The log-in token for the Discord bot account '
+            '(mutually exclusive with DFB_BOT_TOKEN_FILE)'
+        )
     )
 
-    token_file = _environ.var(
+    token_file = environ.var(
         None,
-        help = 'The path to a file containing the log-in token (takes precedence)'
+        help = (
+            'The path to a file containing the log-in token '
+            '(mutually exclusive with DFB_BOT_TOKEN)'
+        )
     )
 
-    async def read_token(self):
-        """Read the token from the config, possibly from the filesystem."""
-        if self.token_file is not None:
-            async with open(self.token_file, 'r') as token_file:
-                return (await token_file.read()).strip()
-
-        return self.token
-
-@_environ.config
-class DatabaseConfig:
-    url = _environ.var(
-        'postgresql://localhost',
-        help = "URL for the database connection"
-    )
-
-@_environ.config
-class LogConfig:
-    level = _environ.var('INFO', help = 'The minimum log level')
-
-@_environ.config(prefix = 'DFB')
-class Config:
-    bot = _environ.group(BotConfig)
-    database = _environ.group(DatabaseConfig)
-    log = _environ.group(LogConfig)
-
-    @bot.validator
-    def _bot_token_valid(self,  attribute, value):
-        if value.token is None and value.token_file is None:
+    @token.validator
+    def _token_valid(self,  attribute, value):
+        if self.token is None and self.token_file is None:
             raise ValueError(
                 'One of DFB_BOT_TOKEN or DFB_BOT_TOKEN_FILE is required'
             )
 
-        if value.token is not None and value.token_file is not None:
+        if self.token is not None and self.token_file is not None:
             raise ValueError(
                 'Only one of DFB_BOT_TOKEN or DFB_BOT_TOKEN_FILE may be set'
             )
+
+    async def read_token(self):
+        """Read the token from the config, possibly from the filesystem."""
+        if self.token_file is not None:
+            async with aiofiles.open(self.token_file, 'r') as token_file:
+                return (await token_file.read()).strip()
+
+        return self.token
+
+@environ.config
+class DatabaseConfig:
+    url = environ.var(help = "URL for the database connection")
+
+@environ.config
+class LogConfig:
+    level = environ.var('INFO', help = 'The minimum log level')
+
+@environ.config(prefix = 'DFB')
+class Config:
+    bot = environ.group(BotConfig)
+    database = environ.group(DatabaseConfig)
+    log = environ.group(LogConfig)
 
