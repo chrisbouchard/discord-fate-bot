aca7f5d3ad5a6ce78ea33aeff5fd7d26729bbf9a
Chris Bouchard <chris@upliftinglemma.net>
2020-03-24 22:51:14 -0400

    Add initial log statement to indicate start of app

--- discord_fate_bot/__main__.py
+++ discord_fate_bot/__main__.py
@@ -5,7 +5,11 @@ from .bot import make_bot, run_bot
 from .config import Config
 from .database import connect_database
 
+logger = logging.getLogger(__name__)
+
 async def main():
+    logger.info('Starting Discord Fate Bot...')
+
     config = Config.from_environ()
     config.log.apply_global()
 
