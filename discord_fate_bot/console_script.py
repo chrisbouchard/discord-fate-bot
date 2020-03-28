4db8f847a6d57b7191a2bb1d9a5063f67683511f
Chris Bouchard <chris@upliftinglemma.net>
2020-03-27 21:10:37 -0400

    Rip out all the database stuff
    :(

--- discord_fate_bot/console_script.py
+++ discord_fate_bot/console_script.py
@@ -3,7 +3,6 @@ import logging
 
 from .bot import make_bot, run_bot
 from .config import Config
-from .database import connect_database
 from .logging import configure_logging
 
 logger = logging.getLogger(__name__)
@@ -16,9 +15,7 @@ def main():
 
     async def _main():
         bot = make_bot(config)
-
-        async with connect_database(bot):
-            await run_bot(bot)
+        await run_bot(bot)
 
     aiorun.run(_main(), stop_on_unhandled_errors=True)
 
--- discord_fate_bot/migrations/console_script.py
+++
@@ -1,9 +0,0 @@
-import alembic.config
-import importlib.resources
-import sys
-
-def main():
-    with importlib.resources.path('discord_fate_bot.migrations', 'alembic.ini') as path:
-        alembic_args = ['-c', str(path), *sys.argv[1:]]
-        alembic.config.main(argv=alembic_args)
-
