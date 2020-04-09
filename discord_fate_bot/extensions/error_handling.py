3a40bff3eaddd23a558b3ac2b0c0fa759782d792
Chris Bouchard <chris@upliftinglemma.net>
2020-04-09 00:45:35 -0400

    Add error handling for CommandNotFoundError

--- discord_fate_bot/extensions/error_handling.py
+++ discord_fate_bot/extensions/error_handling.py
@@ -1,7 +1,7 @@
 import logging
 import textwrap
 
-from discord.ext.commands import Cog, CommandInvokeError, UserInputError
+from discord.ext.commands import Cog, CommandNotFound, CommandInvokeError, UserInputError
 
 from ..emojis import random_error_emoji
 from ..scenes import AspectIdError, NoCurrentSceneError
@@ -16,45 +16,60 @@ def setup(bot):
 class ErrorHandlingCog(Cog):
     @Cog.listener()
     async def on_command_error(self, ctx, error):
-        # Commands wrap their exceptions with this one.
-        if isinstance(error, CommandInvokeError):
-            error = error.__cause__
+        try:
+            # Commands wrap their exceptions with this one.
+            if isinstance(error, CommandInvokeError):
+                error = error.__cause__
 
-        if isinstance(error, UserInputError):
-            message = (
-                f"Sorry, I didn't understand:\n\n" \
-                f"{quote(ctx.message)}\n\n" \
-                f"{punctuate(error)}"
-            )
-            await send_error_message(ctx, message, help_separator=' ')
-        elif isinstance(error, AspectIdError):
-            message = (
-                f"Sorry, there's no aspect in the current scene with id " \
-                f"{error.aspect_id}."
-            )
-            await send_error_message(ctx, message)
-        elif isinstance(error, NoCurrentSceneError):
-            message = (
-                f"Sorry, there's no current scene in this channel. You can " \
-                f"start a new scene using **!scene new**."
-            )
-            await send_error_message(ctx, message)
-        elif isinstance(error, ValidationError):
-            message = f"{error}:\n\n"
-            message += '\n'.join(
-                f'    •  {complaint}'
-                for complaint in error.complaints
-            )
-            await send_error_message(ctx, message)
-        else:
-            message = f"Sorry, something unexpected went wrong."
-            await send_error_message(ctx, message)
-            raise error
+            if isinstance(error, CommandNotFound) or isinstance(error, UserInputError):
+                message = (
+                    f"Sorry, I didn't understand:\n\n" \
+                    f"{quote(ctx.message)}\n\n" \
+                    f"{punctuate(error)}"
+                )
+                await send_error_message(ctx, message, help_separator=' ')
+            elif isinstance(error, AspectIdError):
+                message = (
+                    f"Sorry, there's no aspect in the current scene with id " \
+                    f"{error.aspect_id}."
+                )
+                await send_error_message(ctx, message)
+            elif isinstance(error, NoCurrentSceneError):
+                message = (
+                    f"Sorry, there's no current scene in this channel. You can " \
+                    f"start a new scene using **!scene new**."
+                )
+                await send_error_message(ctx, message)
+            elif isinstance(error, ValidationError):
+                message = f"{error}:\n\n"
+                message += '\n'.join(
+                    f'    •  {complaint}'
+                    for complaint in error.complaints
+                )
+                await send_error_message(ctx, message)
+            else:
+                message = f"Sorry, something unexpected went wrong."
+                await send_error_message(ctx, message)
+                raise error
+        except Exception as ex:
+            # This whole method is basically a catch clause, but we don't get
+            # implicit exception chaining. So let's just add our own explicit
+            # chaining.
+            if error == ex:
+                # Don't try to chain the exception to itself. This gives us an
+                # escape hatch if we need to re-raise the exception.
+                raise
+            raise ex from error
 
 async def send_error_message(ctx, message, *, help_separator='\n\n'):
     """Send an error message with our standard boilerplate."""
     emoji = random_error_emoji()
-    help_command = '!help ' + format_command_for_help(ctx.command)
+
+    if ctx.command:
+        help_command = '!help ' + format_command_for_help(ctx.command)
+    else:
+        help_command = '!help'
+
     final_message = (
         f"{emoji}  {ctx.author.mention} {message}{help_separator}" \
         f"Try **{help_command}** for more information. "\
